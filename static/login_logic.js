b0cbef94dbcdd36606da0d752ada66ca5ed64965
TrisesaSadewa <115542753+TrisesaSadewa@users.noreply.github.com>
2026-01-14 14:18:52 +0700

    Update login_logic.js

--- static/login_logic.js
+++ static/login_logic.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 const SUPABASE_URL = 'https://hwoelsconqsybftgdxft.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_dMAyCUVPT2LK-NHu5NYeTg_zFkfSD8R'; // Public Key
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3b2Vsc2NvbnFzeWJmdGdkeGZ0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODIyNzE5MiwiZXhwIjoyMDgzODAzMTkyfQ.HDdmU4HATVu1ykNOjBjRydf6aJ6qm2xolmBWyPJprqY';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -91,4 +91,5 @@ function showError(msg) {
 
 function hideError() {
     errorMsg.classList.add('hidden');
+
 }
